0d4eb59eda883ae3661e83ed24101796fe917c6c
Serina Ko <serinako08@gmail.com>
2018-10-12 14:48:50 +1100

    wire up FlatList component and style the list

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
+  renderItem({ item }) {
+    return <ListItem library={item} />;
+  }
+
   render() {
     console.log(this.props);
-    return;
+    return (
+      <FlatList
+        data={this.props.libraries}
+        renderItem={this.renderItem}
+        keyExtractor={(library) => library.id}
+      />
+    );
   }
 }
 
