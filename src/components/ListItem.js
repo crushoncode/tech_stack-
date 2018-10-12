cfa0d7ee806f9365411bd86f548ae27d21b75b41
Serina Ko <serinako08@gmail.com>
2018-10-12 15:42:35 +1100

    expand a row with description

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,6 +5,13 @@ import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  renderDescription() {
+    const { library, selectedLibraryId } = this.props;
+    if (library.id === selectedLibraryId) {
+      return <Text>{library.description}</Text>;
+    }
+  }
+
   render() {
     const { titleStyle } = styles;
     const { id, title } = this.props.library;
@@ -15,6 +22,7 @@ class ListItem extends Component {
           <CardSection>
             <Text style={titleStyle}>{title}</Text>
           </CardSection>
+          {this.renderDescription()}
         </View>
       </TouchableWithoutFeedback>
     );
@@ -28,7 +36,11 @@ const styles = {
   }
 };
 
+const mapStateToProps = (state) => {
+  return { selectedLibraryId: state.selectedLibraryId };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   actions
 )(ListItem);
